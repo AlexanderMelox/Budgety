39a47a43d2bc85358a71d786ac026191733d38c4
Alexander Melo <alexandermelox@gmail.com>
2018-04-05 13:18:37 -0400

    added all modules

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const controller = (function(budgetCtrl, UICtrl) {
+
+  document.querySelector('.add__btn').addEventListener('click', function() {
+      
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the new item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  });
+
+  document.addEventListener('keypress', function(event) {
+
+    if (event.keyCode === 13 || event.which === 13) {
+      console.log('ENTER was pressed.');
+    }
+
+  });
+
+})(budgetController, UIController); 
---
+++ js/uiController.js
@@ -0,0 +1,5 @@
+const UIController = (function() {
+
+  // Some code
+
+})();
