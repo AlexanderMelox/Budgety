725b4aab76de82255b39cefa79f55bc7b319fac0
Alexander Melo <alexandermelox@gmail.com>
2018-04-06 17:36:49 -0400

    update budget controller to test if description is empty or value is a number and greater than 0

--- js/app.js
+++ js/app.js
@@ -8,24 +8,34 @@ const controller = (function(budgetCtrl, UICtrl) {
     document.addEventListener('keypress', event => event.keycode === 13 || event.which === 13 ? ctlrAddItem() : null );
   };
 
+  const updateBudget = () => {
+
+    // 1. Calculate the budget
+
+    // 2. Return the budget
+
+    // 3. Display the budget on the UI
+
+  }
 
   const ctlrAddItem = () => {
 
     // 1. Get the field input data
     const input = UICtrl.getInput();
 
-    // 2. Add the item to the budget controller
-    const newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      const newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-    // 3. Add the new item to the UI
-    UICtrl.addListItem(newItem, input.type);
+      // 3. Add the new item to the UI
+      UICtrl.addListItem(newItem, input.type);
 
-    // 4. Clear the fields
-    UICtrl.clearFields();
+      // 4. Clear the fields
+      UICtrl.clearFields();
 
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
+      // 5. Calculate and update budget
+      updateBudget();
+    }
 
   };
 
--- js/uiController.js
+++ js/uiController.js
@@ -13,7 +13,7 @@ const UIController = (function() {
     getInput: () => {
       const type = document.querySelector(DOMStrings.inputType).value; // Will be either inc or exp
       const description = document.querySelector(DOMStrings.inputDescription).value;
-      const value = document.querySelector(DOMStrings.inputValue).value;
+      const value = parseFloat(document.querySelector(DOMStrings.inputValue).value);
 
       return {
         type,
